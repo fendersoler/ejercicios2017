10873c08735c9d453ef936da7ff93a4b58b6c721
ifender <irenefender@hotmail.com>
2017-10-13 11:06:57 +0200

    ejercicio 7

---
+++ Modulo7/src/ejercicio72/Array.java
@@ -0,0 +1,60 @@
+package ejercicio72;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Array {
+
+	int [] array;
+	
+	public Array(){
+		this.array = new int [6];		
+	}	
+
+	public void rellenar (int aleatorio){
+		Arrays.fill(array, aleatorio);
+	}
+
+	public void rellenar (){
+		for (int i = 0; i < array.length; i++) {
+			array[i] = ThreadLocalRandom.current().nextInt(1, 50);		
+			
+			for (int j = 0; j < array.length; j++) {
+				if(i > j){
+					if(array[i]==array[j]){
+						array[i] = ThreadLocalRandom.current().nextInt(1, 50);		
+					}
+				}
+			}
+		}		
+	}
+
+	public void ordenar (){
+		Arrays.sort(array);
+	}
+
+	public void visualizar (){		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Posición [" + i +"] = "+ array[i]);
+		}		
+	}
+	
+	public static void main (String args []){
+		Array miArray = new Array();
+		
+		System.out.println("Rellenar con un aleatorio");
+		int aleatorio = ThreadLocalRandom.current().nextInt(1, 50);
+		miArray.rellenar(aleatorio);				
+		miArray.visualizar();
+		System.out.println("----");
+		
+		System.out.println("Rellenar sin repetir");
+		miArray.rellenar();
+		miArray.visualizar();
+		System.out.println("----");
+		
+		System.out.println("Ordenar");
+		miArray.ordenar();
+		miArray.visualizar();
+	}
+}
