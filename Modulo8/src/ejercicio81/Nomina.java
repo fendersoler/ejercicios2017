98419bd668afcd3f661f829701fc7720e0d464d8
ifender <irenefender@hotmail.com>
2017-10-18 13:41:45 +0200

    commit 18 oct 17

---
+++ Modulo8/src/ejercicio81/Nomina.java
@@ -0,0 +1,8 @@
+package ejercicio81;
+
+public interface Nomina {
+	
+	public float calcularAntiguedadMensual();
+	public float calcularComplementosMensuales();
+	public float calcularSueldoBrutoMensual();
+}
---
+++ Modulo8/src/ejercicio82/LimitesSistema.java
@@ -0,0 +1,11 @@
+package ejercicio82;
+
+public interface LimitesSistema {
+
+	int XMAX = 300;
+	int XMIN = 0;
+	int YMAX = 300;
+	int YMIN = 0;	
+	
+	public void pintar();
+}
